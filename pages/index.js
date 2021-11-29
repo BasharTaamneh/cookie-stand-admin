218cd16c196a749a89705cdce80d5f818f3fe901
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-11-29 22:39:24 +0200

    first commit

--- pages/index.js
+++ pages/index.js
@@ -4,16 +4,24 @@ export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
-        <title>Create Next App</title>
+        <title>Cookie Stand Admin</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col items-center justify-center flex-1 w-full px-20 text-center">
-        
-      </main>
+      <main className="h-screen">
 
-      <footer className="flex items-center justify-center w-full h-24 border-t">
+        <header className="fixed inset-x-0 top-0 flex justify-between p-4 bg-green-500 border-b">
+          <h1 className="text-4xl font-semibold">
+            Cookie Stand Admin
+          </h1>
+        </header>
+
         
+        <p className="justify-center">Report table coming soon</p>
+
+      </main>
+      <footer className="fixed inset-x-0 bottom-0 flex justify-between p-3 bg-green-600 border-t">
+        <b className="text-sm font-normal"> &copy; 2021 </b>
       </footer>
     </div>
   )
