824d36fd63c715a04dc21702e246dabde8257aea
bashar adel taamneh <bashartaamneh55@gmail.com>
2021-11-30 04:10:56 +0200

    React, Next.js & TailwindCSS created

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # Welcome to React, Next.js & TailwindCSS
+
+* React is great! And though it does A LOT, it’s not a full framework. In other words, many common tasks are not handled out of the box by React. So it’s up to us devs to make some decisions about how to use React in combination with other tools.
+
+---
+
+## Description
+
+- built using React Next.js, and Tailwind CSS.
+
+---
+
+## Change log
+
+- **Cookie Stand Admin v1**  _completed class_37_  29-11-2021
+
+---
+
+## [PR](https://github.com/BasharTaamneh/cookie-stand-admin/pull/1)
--- pages/index.js
+++ pages/index.js
@@ -1,28 +1,54 @@
 import Head from 'next/head'
+import Form from "./Form"
+import { useState } from 'react'
 
 export default function Home() {
+
+  const [store, setStore] = useState();
+
+  const addStore = (newStore) => {
+    setStore(JSON.stringify(newStore));
+
+  }
   return (
+
     <div className="flex flex-col items-center justify-center min-h-screen py-2">
       <Head>
         <title>Cookie Stand Admin</title>
-        <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="h-screen">
+      <main className=" justify-content">
 
-        <header className="fixed inset-x-0 top-0 flex justify-between p-4 bg-green-500 border-b">
-          <h1 className="text-4xl font-semibold">
+        <header class="fixed inset-x-0 top-0 flex justify-between p-4 bg-green-500 border-b ">
+          <h1 class="text-4xl font-semibold">
             Cookie Stand Admin
+
           </h1>
         </header>
 
-        
-        <p className="justify-center">Report table coming soon</p>
+        {/* //////////////////////////////////////////////////////// */}
+
+        <Form onSubmit={addStore} />
+
+        <br />
+
+        <div className="flex justify-center p-1 mt-6">
+          <p> Report table coming soon ...</p>
+        </div>
+
+        <dev className="flex justify-center p-2 mt-6">
+          <pre>{store}</pre>
+
+        </dev>
+
+        {/* //////////////////////////////////////////////////////// */}
 
       </main>
-      <footer className="fixed inset-x-0 bottom-0 flex justify-between p-3 bg-green-600 border-t">
+
+      <footer className="fixed inset-x-0 bottom-0 flex justify-between p-3 mt-8 bg-green-600 border-t">
         <b className="text-sm font-normal"> &copy; 2021 </b>
       </footer>
+
     </div>
   )
 }
